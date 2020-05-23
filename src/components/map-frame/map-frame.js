0400915e253c8300acb5625a6448553d8f12d159
jacobhanson <jhanreg11@gmail.com>
2020-05-23 11:21:06 -0700

    Updates

--- src/components/map-frame/map-frame.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './map-frame.css'
-
-const MapFrame = props => (
-	<iframe title="Char's Location" className="map-frame" id={props.id} src={props.embedURL}></iframe>
-)
-
-export default MapFrame
--- src/components/navbar/backdrop/backdrop.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import './backdrop.css'
-const Backdrop = props => (
-  <div className="backdrop" onClick={props.click}/>
-)
-
-export default Backdrop
