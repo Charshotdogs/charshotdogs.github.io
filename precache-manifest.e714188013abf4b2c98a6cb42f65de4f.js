83a1b168cd6691c8e1270b5fc096d7a80b1967f5
jhanreg11 <jhanreg11@gmail.com>
2020-05-26 19:57:32 +0000

    Deploying to master from  @ 456046bdc2186b911c7493edfd7b52c7a0bdc2c9 🚀

---
+++ precache-manifest.e714188013abf4b2c98a6cb42f65de4f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "807a3aba7c06b03bd771f273d2615e7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4aa3f1f6ad888d920f9d",
+    "url": "/static/css/main.8d4d3934.chunk.css"
+  },
+  {
+    "revision": "9fe8c523c98371001960",
+    "url": "/static/js/2.3a329f3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3a329f3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4aa3f1f6ad888d920f9d",
+    "url": "/static/js/main.52015b8e.chunk.js"
+  },
+  {
+    "revision": "342e23b3f13bac2f798b",
+    "url": "/static/js/runtime-main.f5ec108c.js"
+  },
+  {
+    "revision": "097b77494452f9352630bcf79697db4d",
+    "url": "/static/media/ig_icon_color.097b7749.png"
+  },
+  {
+    "revision": "c570003aba942cab2c94b9fb3365519c",
+    "url": "/static/media/landing.c570003a.webp"
+  }
+]);
