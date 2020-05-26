1ec4f54302a293c1d674e543ccd0b1b7a890c353
jacobhanson <jhanreg11@gmail.com>
2020-05-25 23:19:33 -0700

    Added about paragraph

--- src/pages/about-page/about-page.js
+++ src/pages/about-page/about-page.js
@@ -4,7 +4,13 @@ import './about-page.css'
 
 const AboutPage = () => (
 	<Section title="About" id="about">
-			<div className="body-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut molestie, justo in viverra molestie, nibh velit ullamcorper risus, eget varius elit lectus quis ex. Duis quis neque mollis, interdum dolor a, dictum ex. Duis at eros ac ipsum congue cursus a quis enim. Suspendisse non nibh vel enim blandit maximus eu sit amet tortor. Vestibulum dictum blandit pretium. Aliquam erat volutpat. Curabitur ullamcorper orci eu nunc efficitur, non congue libero varius. Nullam ullamcorper tempus quam, hendrerit congue nisl rhoncus in. Nullam a pretium dolor, sit amet consectetur enim.</div>
+			<div className="body-text">
+			I bought Char's Hot Dogs January 4, 2016 from Charlene Herd that had the business for almost 20 years.
+			I had always wanted to own my own restaurant after working in restaurants for almost 30 years I finally did it.
+			I knew restaurant work was my thing when I was young and use to play restaurant with my grandma and grandpa. I wish that they could be here and see that I finally did it.
+			Working in Benicia inside the historic Tannery Building has been a dream, and I hope to continue serving the city for many more years.
+			Thank you for your support!
+			</div>
 	</Section>
 )
 
