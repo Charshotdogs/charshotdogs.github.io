f35d141a2e2958526826e718db6772c63452e4d4
jacobhanson <jhanreg11@gmail.com>
2020-05-26 11:23:45 -0700

    Updates

---
+++ precache-manifest.31212fc0a86a81839ae424755d771624.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "008bebaeb285e98da444f705ffebd9a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ee5ce8dfc7cf653644b",
+    "url": "/static/css/main.42218a61.chunk.css"
+  },
+  {
+    "revision": "9fe8c523c98371001960",
+    "url": "/static/js/2.3a329f3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3a329f3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ee5ce8dfc7cf653644b",
+    "url": "/static/js/main.91bacf08.chunk.js"
+  },
+  {
+    "revision": "342e23b3f13bac2f798b",
+    "url": "/static/js/runtime-main.f5ec108c.js"
+  },
+  {
+    "revision": "097b77494452f9352630bcf79697db4d",
+    "url": "/static/media/ig_icon_color.097b7749.png"
+  },
+  {
+    "revision": "c570003aba942cab2c94b9fb3365519c",
+    "url": "/static/media/landing.c570003a.webp"
+  }
+]);
