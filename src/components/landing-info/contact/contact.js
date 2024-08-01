25573e4ec96d777e90447b8eb2a96264e9cba7b2
Charshotdogs <64802027+Charshotdogs@users.noreply.github.com>
2024-08-01 15:45:14 -0700

    Update contact.js

--- src/components/landing-info/contact/contact.js
+++ src/components/landing-info/contact/contact.js
@@ -18,10 +18,13 @@ const Contact = props => (
 		131 First Street, Benicia CA
 		<br/>
 		<br/>
-		<a rel="noopener noreferrer" href="https://www.google.com/maps/place/Char's+Hot+Dogs/@38.046162,-122.161675,15z/data=!4m5!3m4!1s0x0:0x149549adb869eb1e!8m2!3d38.0461618!4d-122.1616748?hl=en" target="_blank">view map</a>
+		<a rel="noopener noreferrer" href="https://www.google.com/maps/place/Char's+Hot+Dogs/@38.046162,-122.161675,15z/data=!4m5!3m4!1s0x0:0x149549adb869eb1e!8m2!3d38.0461618!4d-122.1616748?hl=en" target="_blank">View map</a>
 		<br/>
 		<br/>
 		707 745 4476
+		<br/>
+		<br/>
+		<a rel="noopener noreferrer" target="_blank" href="https://order.spoton.com/so-chars-hot-dogs-18460/benicia-ca/65fca40b0fb631003f7fc6de">Order online</a>
 		<br/>
 		<br/>
 		<SocialLinks items={Links}/>
